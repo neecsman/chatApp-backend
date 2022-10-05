01ae24b0d156a2c9f817e56b776f12e7139b5ff3
Nikita <nikappleid@yandex.ru>
2022-10-05 18:00:56 +0300

    Подправил userDto для оттображения инфы на фронте

--- src/dtos/user.dto.ts
+++ src/dtos/user.dto.ts
@@ -2,10 +2,14 @@ export default class UserDto {
   email: string;
   id: string;
   isActivated: boolean;
+  avatarPath: string;
+  fullname: string;
 
   constructor(entity: any) {
     this.email = entity.email;
     this.id = entity.id;
     this.isActivated = entity.isActivated;
+    this.avatarPath = entity.avatarPath;
+    this.fullname = entity.fullname;
   }
 }
